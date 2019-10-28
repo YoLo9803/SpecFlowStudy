fc08b9794dc8088f5856427c789b2a9a8daebfef
chris <f98989000@gmail.com>
2019-10-28 14:17:00 +0800

    solution initial

---
+++ SpecFlowStudy/SpecFlowStudy.UiTests.WebDriver/NUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SpecFlowStudy.UiTests.WebDriver
+{
+    public class NUnitTest
+    {
+        [Test]
+        public void TestNUnit()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ SpecFlowStudy/SpecFlowStudy.UiTests.WebDriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecFlowStudy.UiTests.WebDriver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpecFlowStudy.UiTests.WebDriver")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db9613e1-c208-4621-96f9-bba4a733bcce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
